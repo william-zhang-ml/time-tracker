6f36f80ec9cc50c290e1aea92afd11183db832d9
William Zhang <william.zhang.ml@gmail.com>
2024-06-25 21:00:48 -0400

    add button class that implements a tooltip

--- app.py
+++ app.py
@@ -37,15 +37,50 @@ def get_user_input(choices: Iterable[str]) -> str:
     return radio_out.get()
 
 
+class ResponseButton(ttk.Button):
+    """Button that keeps track of user response, lets user change response. """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.bind("<Enter>", lambda _: self.showtip())
+        self.bind("<Leave>", lambda _: self.hidetip())
+        self.tooltip = None
+
+    def showtip(self) -> None:
+        """Create a new tooltip window. """
+        if self.tooltip:
+            return  # already activated
+        self.tooltip = tk.Toplevel(self)
+        self.tooltip.wm_overrideredirect(1)
+        self.tooltip.wm_geometry(
+            f'+{self.winfo_rootx()}+{self.winfo_rooty() + 25}'
+        )
+        label = ttk.Label(
+            self.tooltip,
+            text=self.cget('text'),
+            justify=tk.LEFT,
+            background="#ffffe0",
+            relief=tk.SOLID,
+            borderwidth=1
+        )
+        label.pack(ipadx=1)
+
+    def hidetip(self) -> None:
+        """Destroy the tooltip window. """
+        if self.tooltip:
+            self.tooltip.destroy()
+            self.tooltip = None
+
+
 if __name__ == '__main__':
-    # Create main window
+    # Create progress tracker window
     root = tk.Tk()
     root.title('Progress')
-    root.withdraw()
 
-    # Get user input
-    resp = get_user_input(['Option 1', 'Option 2', 'Option 3'])
-    print(resp)
+    for _ in range(5):
+        # Get user input
+        resp = get_user_input(['Option 1', 'Option 2', 'Option 3'])
 
-    # Destroy the main window
-    root.destroy()
+        # Pack into progress tracker
+        ResponseButton(root, text=resp).pack(padx=20, pady=5)
+
+    root.mainloop()
